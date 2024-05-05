cff9658cb16fd62b673cbe423b3341fe0c99c57d
XuanTai125 <163371514+XuanTai125@users.noreply.github.com>
2024-05-05 14:38:48 +0700

    Add files via upload

---
+++ trangchu.js
@@ -0,0 +1,7 @@
+const text = document.getElementById("moving-text");
+
+// Update the text content
+text.textContent = "CHÀO MỪNG ĐẾN VỚI TRANG CÁ NHÂN CỦA TÔI";
+
+// Set the initial position of the text
+text.style.left = "0";
